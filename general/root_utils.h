028dbcbfa0456f2460a18fd9150062978926c3c6
thomas madlener <thomas.madlener@cern.ch>
2016-11-03 23:45:00 +0100

    added some histogram helper functions

--- general/root_utils.h
+++ general/root_utils.h
@@ -20,6 +20,14 @@ bool inheritsFrom(const TObject* obj)
   return obj/*->IsA()*/->InheritsFrom(RT::Class());
 }
 
+/** check if the object inherits from RT and return a pointer to it after a static_cast. */
+template<typename RT> inline
+RT* conditionalCast(TObject* obj)
+{
+  if (!inheritsFrom<RT>(obj)) return nullptr;
+  return static_cast<RT*>(obj);
+}
+
 /** get from Root file. Using dynamic cast to get throught the inheritance stuff correctly. */
 template<typename T> inline
 T* getFromFile(TFile* f, const std::string& name)
@@ -51,6 +59,29 @@ template<typename T> inline
 T* getFromCanvas(const TCanvas* c, const std::string& name)
 {
   return dynamic_cast<T*>(c->FindObject(name.c_str()));
+}
+
+/** clone the passed TObject with a new name (defaults to empty string). */
+template<typename T> inline
+T* clone(const T* obj, const std::string& name = "")
+{
+  return static_cast<T*>(obj->Clone(name.c_str()));
+}
+
+/** scale the passed histogram such, that it's integral will be equal to f. */
+template<typename H> inline
+void selfScale(H* h, const double f = 1.0)
+{
+  h->Scale(f / h->Integral());
+}
+
+/** get a scaled clone of the passed histogram. */
+template<typename H> inline
+H* cloneScale(const H* hist, const double f = 1.0)
+{
+  H* h = clone(hist);
+  selfScale(h, f);
+  return h;
 }
 
 #endif
