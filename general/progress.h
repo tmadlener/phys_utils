c9fa3424954fdd5acc920d8080ee1e37e1d46b0c
thomas madlener <thomas.madlener@cern.ch>
2016-11-09 13:59:30 +0100

    introduced small typedef for less typing effort

--- general/progress.h
+++ general/progress.h
@@ -41,13 +41,16 @@ std::ostream& operator<<(std::ostream& os, const SimpleTime& time)
   return os;
 }
 
+/** typedef to have a common type throughout all the uses. (And also less typing effort) */
+using ProgressClock = std::chrono::high_resolution_clock;
+
 /**
  * Print the progress of a process assuming that there are N total steps and i is the current step.
  * Also prints the elapsed time assuming that the processing startet at the startTime and an estimate of the remaining time.
  *
  * The nPrints variable controls how many printouts there will be and the os can be used to redirect the printout to any ostream.
  */
-template<typename ClockType = std::chrono::high_resolution_clock>
+template<typename ClockType = ProgressClock>
 void printProgress(const size_t& i, const size_t& N, const typename ClockType::time_point startTime, const size_t& nPrints = 100,
                    std::ostream& os = std::cout)
 {
