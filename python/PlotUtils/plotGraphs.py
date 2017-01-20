5df43f539adf0bebc52920596a8ac049fa406177
thomas madlener <thomas.madlener@cern.ch>
2017-01-20 11:17:22 +0100

    moved plotGraphs.py to python dir and use utils.recurse now

--- python/PlotUtils/plotGraphs.py
+++ python/PlotUtils/plotGraphs.py
@@ -1,26 +1,10 @@
-import ROOT as r
+#!/usr/bin/env python
+
 import argparse
 import json
 import math
 
-def recurseOnFile(f, func, dirFunc = None):
-    """
-    Generic root file recursion function that traverses all TDirectories that can be found in a TFile.
-    For every key that is found in the file it is checked if it inherits from TDirectory and if not the
-    function passed in via the func paramter is executed.
-    * f is the TFile or TDirectory to recurse on.
-    * func is any function that takes a TObject as its single argument.
-    * dirFunc is any function taking a TDirectory as its single argument that is executed on each directory but
-      should be outside of the recursion (e.g. obtaining the current path)
-    """
-    for key in f.GetListOfKeys():
-        obj = key.ReadObj()
-        if not obj.InheritsFrom('TDirectory'):
-            func(obj)
-        else:
-            dirFunc(obj)
-            recurseOnFile(obj, func, dirFunc)
-
+from utils.recurse import recurseOnFile
 
 class GraphCollector(object):
     """
@@ -180,18 +164,19 @@ def removeNans(graph):
 
 
 """
-ROOT setup
-"""
-r.gROOT.ProcessLine("gErrorIgnoreLevel = 1001")
-r.gROOT.SetBatch()
-
-
-"""
 Arg parser
 """
 parser = argparse.ArgumentParser(description='script for plotting TGraphs')
 parser.add_argument('jsonFile', help='json file containing the main configuration that can be overriden by other args')
 args = parser.parse_args()
+
+
+"""
+ROOT setup
+"""
+import ROOT as r
+r.gROOT.ProcessLine("gErrorIgnoreLevel = 1001")
+r.gROOT.SetBatch()
 
 
 """
