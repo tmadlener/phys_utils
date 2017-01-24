19e882e8366c61eaf2580fa4f9ddb2cc68b614e6
thomas madlener <thomas.madlener@cern.ch>
2017-01-24 12:09:09 +0100

    added plotting of pull scatter plot as well as verbosity flag to steer progress printout

--- python/PolUtils/plotReferenceMapsTest.py
+++ python/PolUtils/plotReferenceMapsTest.py
@@ -15,75 +15,93 @@ plt.rcParams['axes.labelsize'] = 16
 plt.rc("text",usetex=True)
 plt.rc("font", family="serif")
 
+beVerbose = False # turn progress output off by default
 
-def collectDataFile(fn):
-    """
-    Collect the data from one file
-    """
-    f = TFile.Open(fn)
-    tree = f.Get("refMapTest") # hardcoded
+class FilesData:
+    """helper class for holding all data from all files."""
 
-    N = tree.GetEntries()
-    lthInj = np.zeros(N, dtype=float)
-    lphInj = np.zeros(N, dtype=float)
-    ltpInj = np.zeros(N, dtype=float)
+    def __init__(self):
+        """Create empty list for each parameter"""
+        self.lthInj = []
+        self.lphInj = []
+        self.ltpInj = []
 
-    lthFit = np.zeros(N, dtype=float)
-    lphFit = np.zeros(N, dtype=float)
-    ltpFit = np.zeros(N, dtype=float)
+        self.lthInjErr = []
+        self.lphInjErr = []
+        self.ltpInjErr = []
 
-    # read entries into arrays
-    i=0
-    for entry in tree:
-        lthInj[i] = entry.lthInj
-        lphInj[i] = entry.lphInj
-        ltpInj[i] = entry.ltpInj
+        self.lthFit = []
+        self.lphFit = []
+        self.ltpFit = []
 
-        lthFit[i] = entry.lthFit
-        lphFit[i] = entry.lphFit
-        ltpFit[i] = entry.ltpFit
+    def initTree(self, tree):
+        """Setup branches in new list for TTree to read"""
+        N = tree.GetEntries()
+        self.lthInj.append(np.zeros(N, dtype=float))
+        self.lphInj.append(np.zeros(N, dtype=float))
+        self.ltpInj.append(np.zeros(N, dtype=float))
 
-        i += 1
+        self.lthInjErr.append(np.zeros(N, dtype=float))
+        self.lphInjErr.append(np.zeros(N, dtype=float))
+        self.ltpInjErr.append(np.zeros(N, dtype=float))
 
-    f.Close()
+        self.lthFit.append(np.zeros(N, dtype=float))
+        self.lphFit.append(np.zeros(N, dtype=float))
+        self.ltpFit.append(np.zeros(N, dtype=float))
 
-    return [lthInj, lphInj, ltpInj, lthFit, lphFit, ltpFit]
+
+    def loopTree(self, tree):
+        """Loop over all events in tree and read entries into np arrays"""
+        i=0
+        for entry in tree:
+            self.lthInj[-1][i] = entry.lthInj
+            self.lphInj[-1][i] = entry.lphInj
+            self.ltpInj[-1][i] = entry.ltpInj
+
+            self.lthInjErr[-1][i] = entry.lthInjErr
+            self.lphInjErr[-1][i] = entry.lphInjErr
+            self.ltpInjErr[-1][i] = entry.ltpInjErr
+
+            self.lthFit[-1][i] = entry.lthFit
+            self.lphFit[-1][i] = entry.lphFit
+            self.ltpFit[-1][i] = entry.ltpFit
+
+            i += 1
 
 
 def collectData(infiles):
     """
     Collect the data from all passed files
     """
-    lthInj = []
-    lphInj = []
-    ltpInj = []
-    lthFit = []
-    lphFit = []
-    ltpFit = []
+    allData = FilesData()
 
-    for f in infiles:
-        fileData = collectDataFile(f[0])
-        lthInj.append(fileData[0])
-        lphInj.append(fileData[1])
-        ltpInj.append(fileData[2])
+    for ifn in infiles:
+        if beVerbose: print("Processing file \'{}\'".format(ifn[0]))
 
-        lthFit.append(fileData[3])
-        lphFit.append(fileData[4])
-        ltpFit.append(fileData[5])
+        f = TFile.Open(ifn[0])
 
-    return [lthInj, lphInj, ltpInj, lthFit, lphFit, ltpFit]
+        tree = f.Get("refMapTest") # hardcoded
+        allData.initTree(tree)
+        allData.loopTree(tree)
+
+    return allData
 
 
-def calcResiduals(inj, fit):
+def calcDifferences(a, b):
     """
-    Calculate the residuals betwenn injected and fitted values
-    for a list of numpy arrays (as returned from collectData)
+    Calculate the differences between a and b, where a and b
+    are both a list of numpy arrays with the same dimensions
+    (i.e. length of list and length of numpy arrays)
     """
-    res = []
-    for (i, f) in zip(inj, fit):
-        res.append(f - i)
+    return [f - i for (i, f) in zip(a, b)]
 
-    return res
+
+def calcRatios(a, b):
+    """
+    Calculate the ratio of a over b, where a and b are both a list of numpy arrays
+    with the same dimensions (i.e. length of list and length of numpy arrays)
+    """
+    return [n / d for (n, d) in zip(a, b)]
 
 
 def createHist(vals, infiles, filename, lsub):
@@ -91,6 +109,8 @@ def createHist(vals, infiles, filename, lsub):
     Save histogram of the passed vals (numpy.array) under the passed filename
     """
     # we want symmetric plots
+    if beVerbose: print("Creating plot \'{}\'".format(filename))
+
     maxval = np.max(np.abs(vals))
 
     colors = ['b', 'r', 'g', 'k', 'c', 'm', 'y'] # can't plot more than 7 in this way
@@ -110,20 +130,26 @@ def createHist(vals, infiles, filename, lsub):
     fig.savefig(filename)
 
 
-def createScat(res, ref, infiles, filename, lsub):
+def createScat(res, ref, infiles, filename, lsub, pulls=False):
     """
-    Create and save a scatter plot of residuals vs reference values
+    Create and save a scatter plot of residuals vs reference values (or pulls vs. reference values)
     """
+    if beVerbose: print("Creating plot \'{}\'".format(filename))
+
     xlabel = r''.join([r'\lambda_{', lsub, r'}^\text{inj}'])
-    ylabel = r''.join([r'(\lambda_{', lsub, r'}^\text{fit} - \lambda_{', lsub, r'}^\text{inj}) / \lambda_{',
-                       lsub, r'}^\text{inj}'])
+    if pulls:
+        ylabel = r''.join([r'(\lambda_{', lsub, r'}^\text{fit} - \lambda_{', lsub,
+                           r'}^\text{inj}) / \sigma_{\lambda_{', lsub, r'}^\text{inj}}'])
+    else:
+        ylabel = r''.join([r'(\lambda_{', lsub, r'}^\text{fit} - \lambda_{', lsub,
+                           r'}^\text{inj}) / \lambda_{', lsub, r'}^\text{inj}'])
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
     colors = ['b', 'r', 'g', 'k', 'c', 'm', 'y'] # can't plot more than 7 in this way
     for (rs, rf, i, c) in zip(res, ref, infiles, colors):
-        ax.scatter(rf, rs/rf, color=c, label=i[1])
+        ax.scatter(rf, rs, color=c, label=i[1])
 
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
@@ -145,10 +171,13 @@ parser.add_argument("--inputfile", "-i", nargs="+", dest="inputfiles", action="a
                     help="add input file to be used (filename + leg entry (can be empty and can contain whitespace))")
 parser.add_argument("--output", "-o", help="Path to which the produced pdfs should be stored.",
                     action="store", dest="outPath")
+parser.add_argument("--verbose", "-v", help="Print some progress messages",
+                    action="store_true", dest="verbose")
 
-parser.set_defaults(outPath="", inputfiles=[])
+parser.set_defaults(outPath="", inputfiles=[], verbose=False)
 args = parser.parse_args()
 
+beVerbose = args.verbose
 
 """
 Script
@@ -159,17 +188,29 @@ gROOT.SetBatch()
 
 infiles = sanitizeInputFileNames(args.inputfiles)
 
-[lthInj, lphInj, ltpInj, lthFit, lphFit, ltpFit] = collectData(infiles)
+data = collectData(infiles)
 
 # calculate residuals
-lthRes = calcResiduals(lthInj, lthFit)
-lphRes = calcResiduals(lphInj, lphFit)
-ltpRes = calcResiduals(ltpInj, ltpFit)
+lthRes = calcDifferences(data.lthInj, data.lthFit)
+lphRes = calcDifferences(data.lphInj, data.lphFit)
+ltpRes = calcDifferences(data.ltpInj, data.ltpFit)
 
 createHist(lthRes, infiles, "".join([args.outPath, "lth_residuals.pdf"]), r"\theta")
 createHist(lphRes, infiles, "".join([args.outPath, "lph_residuals.pdf"]), r"\phi")
 createHist(ltpRes, infiles, "".join([args.outPath, "ltp_residuals.pdf"]), r"\theta\phi")
 
-createScat(lthRes, lthInj, infiles, "".join([args.outPath, "lth_scatter.pdf"]), r"\theta")
-createScat(lphRes, lphInj, infiles, "".join([args.outPath, "lph_scatter.pdf"]), r"\phi")
-createScat(ltpRes, ltpInj, infiles, "".join([args.outPath, "ltp_scatter.pdf"]), r"\theta\phi")
+lthRelRes = calcRatios(lthRes, data.lthInj)
+lphRelRes = calcRatios(lphRes, data.lphInj)
+ltpRelRes = calcRatios(ltpRes, data.ltpInj)
+
+createScat(lthRelRes, data.lthInj, infiles, "".join([args.outPath, "lth_scatter.pdf"]), r"\theta")
+createScat(lphRelRes, data.lphInj, infiles, "".join([args.outPath, "lph_scatter.pdf"]), r"\phi")
+createScat(ltpRelRes, data.ltpInj, infiles, "".join([args.outPath, "ltp_scatter.pdf"]), r"\theta\phi")
+
+lthPulls = calcRatios(lthRes, data.lthInjErr)
+lphPulls = calcRatios(lphRes, data.lphInjErr)
+ltpPulls = calcRatios(ltpRes, data.ltpInjErr)
+
+createScat(lthPulls, data.lthInj, infiles, "".join([args.outPath, "lth_pulls_scatter.pdf"]), r"\theta", True)
+createScat(lphPulls, data.lphInj, infiles, "".join([args.outPath, "lph_pulls_scatter.pdf"]), r"\phi", True)
+createScat(ltpPulls, data.ltpInj, infiles, "".join([args.outPath, "ltp_pulls_scatter.pdf"]), r"\theta\phi", True)
