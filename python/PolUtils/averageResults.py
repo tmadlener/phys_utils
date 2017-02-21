b2336e81d283f70656255da686798a8fec0f652d
thomas madlener <thomas.madlener@cern.ch>
2017-02-21 13:41:55 +0100

    first version of averaging final fit results

---
+++ python/PolUtils/averageResults.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import argparse
+from utils.TGraph_utils import averageGraphs
+from utils.recurse import recurseOnFile, ObjectCollector
+
+
+def getListOfGraphs(fn):
+    """Get the list of the names of all graphs in the passed file"""
+    f = TFile.Open(fn)
+    graphCollector = ObjectCollector("", "TGraphAsymmErrors")
+    recurseOnFile(f, graphCollector)
+    graphNames = [n for n in graphCollector.objects]
+    f.Close()
+    return graphNames
+
+
+def getGraphs(graphNames, files):
+    """Get all graphs from all files and return a dict of lists"""
+    graphs = {}
+    for n in graphNames:
+        graphs[n] = []
+
+    for f in files:
+        for n in graphNames:
+            graphs[n].append(f.Get(n))
+
+    return graphs
+
+
+"""
+Argparse
+"""
+parser = argparse.ArgumentParser(description="script for averaging the same graph from different files")
+parser.add_argument("inputFiles", help="list of input files to process",
+                    nargs="+")
+parser.add_argument("-o", "--ouptutfile", help="File to which the averaged graph should be stored",
+                    dest="outputFile", action="store", default="averageGraphs.root")
+
+args = parser.parse_args()
+
+"""
+Script
+"""
+from ROOT import TFile, TGraphAsymmErrors
+
+# get the list of all graphs from the first file in the list and
+# then use that list to get them from the other files as well (without recursing)
+graphNames = getListOfGraphs(args.inputFiles[0])
+
+# open all the files here to be able to close them again easily
+files = [TFile.Open(n) for n in args.inputFiles]
+graphs = getGraphs(graphNames, files)
+
+
+outputFile = TFile(args.outputFile, 'recreate')
+for n in graphNames:
+    avgGraph = averageGraphs(graphs[n])
+    avgGraph.SetName(n)
+    avgGraph.Write()
+
+outputFile.Write()
+outputFile.Close()
+
+for f in files:
+    f.Close()
--- python/utils/TGraph_utils.py
+++ python/utils/TGraph_utils.py
@@ -10,3 +10,61 @@ def createGraph(x, y, exh, exl, eyh, eyl):
         graph.SetPointError(i, exl[i], exh[i], eyl[i], eyh[i])
 
     return graph
+
+
+def getAvgVal(graphs, i):
+    """Get the average value of all y-values at index i in all the passed graphs."""
+    from numpy import mean, std
+    from ROOT import Double
+    x = Double(0)
+    y = Double(0)
+    vals = []
+    for g in graphs:
+        g.GetPoint(i, x, y)
+        # convert the ROOT.Double to a float to get the value into the list not just a
+        # reference (which would result in a list of equal values when evaluated later)
+        vals.append(float(y))
+
+    return [mean(vals), std(vals)]
+
+
+def getAvgErr(graphs, i):
+    """Get the average error of all the y-values at index i in all the passed graphs."""
+    from numpy import mean
+    return mean([g.GetErrorY(i) for g in graphs])
+
+
+def getXInfo(graph, i):
+    """Get the x information from point i in the passed graph."""
+    from ROOT import Double
+    x = Double(0)
+    y = Double(0)
+    graph.GetPoint(i, x, y)
+    exl = graph.GetErrorXlow(i)
+    exh = graph.GetErrorXhigh(i)
+
+    return [x, exl, exh]
+
+
+def averageGraphs(graphs):
+    """
+    Create a new TGraphAsymmErrors from the passed list of TGraphAsymmErrors
+    where the central values are the average values of all graphs.
+    The uncertainties are computed from adding in quadrature the average of
+    the uncertainties and the std-deviation of the central values
+    """
+    from ROOT import TGraphAsymmErrors
+    from math import sqrt
+    # simply assume that all graphs have the same number of points
+    nPoints = graphs[0].GetN()
+    graph = TGraphAsymmErrors(nPoints)
+    for i in range(0, nPoints):
+        [centralVal, cValSig] = getAvgVal(graphs, i)
+        meanErr = getAvgErr(graphs, i)
+        uncer = sqrt(cValSig**2 + meanErr**2)
+
+        [x, exl, exh] = getXInfo(graphs[0], i) # simply assume it is the same for all
+        graph.SetPoint(i, x, centralVal)
+        graph.SetPointError(i, exl, exh, uncer, uncer)
+
+    return graph
