c0051abb50a56f753014fffe08ac5a562abd651e
thomas madlener <thomas.madlener@cern.ch>
2017-08-14 17:45:48 +0200

    Add ordering to vector-valued input arguments

--- general/ArgParser.h
+++ general/ArgParser.h
@@ -11,6 +11,66 @@
 #include <typeinfo>
 #include <regex>
 
+/**
+ * Due to the fact that the ArgParser reads everything into an unordered_multimap of strings
+ * the order of the user input is not necessarily preserved when obtaining a vector of input
+ * values.
+ *
+ * These flags can be used to specify a given ordering.
+ */
+enum class ValueOrdering {
+  Default, /**< Values as they are present in the map. */
+  Ascending, /**< Values in ascending order (as defined by the less than operator of the type). */
+  Descending /**< Values in descending order (as defined by the greater than operator of the type). */
+};
+
+/**
+ * Class used for actually sorting the parameters.
+ * Needed as a class to be able partially specialize on the ordering of the value.
+ */
+template<ValueOrdering VO>
+class ValueSorter {
+public:
+
+  /**
+   * sorts the passed values respecting the specified ordering.
+   * Goes through sort_impl for tag-dispatching.
+   */
+  template<typename T>
+  static void sort(T& vals) { return sort_impl(vals); }
+
+private:
+  ValueSorter() {};
+
+  /** no-op overload for 'non-sortable' types. */
+  template<typename T>
+  static void sort_impl(T&) { return ; }
+
+  /** overloads for vectors. Will be specialized below for each ordering. */
+  template<typename T>
+  static void sort_impl(std::vector<T>& vals);
+};
+
+template<>
+template<typename T>
+void ValueSorter<ValueOrdering::Default>::sort_impl(std::vector<T>&)
+{
+  return;
+}
+
+template<>
+template<typename T>
+void ValueSorter<ValueOrdering::Ascending>::sort_impl(std::vector<T>& vals)
+{
+  return std::sort(vals.begin(), vals.end());
+}
+
+template<>
+template<typename T>
+void ValueSorter<ValueOrdering::Descending>::sort_impl(std::vector<T>& vals)
+{
+  return std::sort(vals.begin(), vals.end(), [](const T& a, const T& b) { return a > b; });
+}
 
 /**
  * very simple argparser.
@@ -31,11 +91,11 @@ public:
   friend std::ostream& operator<<(std::ostream&, const ArgParser&);
 
   /** Try to get the value associated with the passed key. If the key is not found the defVal will be returned. */
-  template<typename T>
+  template<typename T, ValueOrdering VO = ValueOrdering::Default>
   T getOptionVal(const std::string& key, const T& defVal) const;
 
   /** Try to get the value associated with the passed key. If the key is not found the program is terminated. */
-  template<typename T>
+  template<typename T, ValueOrdering VO = ValueOrdering::Default>
   T getOptionVal(const std::string& key) const;
 
 private:
@@ -73,7 +133,6 @@ private:
   template<typename T>
   std::pair<bool, std::vector<T> > getArg_impl(const std::string& key, std::vector<T>*) const;
 
-
   ParseMap m_argumentMap; /**< The internally used map for storing all flag value pairs. */
 };
 
@@ -105,7 +164,7 @@ std::ostream& operator<<(std::ostream& os, const ArgParser& args)
   return os;
 }
 
-template<typename T>
+template<typename T, ValueOrdering VO>
 T ArgParser::getOptionVal(const std::string& key) const
 {
   auto args = getArg<T>(key);
@@ -116,11 +175,12 @@ T ArgParser::getOptionVal(const std::string& key) const
     exit(1);
   }
 
+  ValueSorter<VO>::sort(args.second);
   return args.second;
 }
 
 
-template<typename T>
+template<typename T, ValueOrdering VO>
 T ArgParser::getOptionVal(const std::string& key, const T& defVal) const
 {
   auto args = getArg<T>(key);
@@ -131,6 +191,7 @@ T ArgParser::getOptionVal(const std::string& key, const T& defVal) const
     return defVal;
   }
 
+  ValueSorter<VO>::sort(args.second);
   return args.second;
 }
 
