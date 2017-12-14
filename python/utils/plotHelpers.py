df52c006726d8c48aff9cb7a8146afa9270961cb
thomas madlener <thomas.madlener@cern.ch>
2017-12-14 16:46:08 +0100

    Refactor to avoid all possible work if not necessary

--- python/utils/plotHelpers.py
+++ python/utils/plotHelpers.py
@@ -12,26 +12,30 @@ _drawCanvas = []
 def _defaultColors():
     from ROOT import TColor
 
-    _rgbcolors = [ # some rgb colors (taken from MATLABs default palette)
-        [0, 0.4470, 0.7410],
-        [0.8500, 0.3250, 0.0980],
-        [0.9290, 0.6940, 0.1250],
-        [0.4940, 0.1840, 0.5560],
-        [0.4660, 0.6740, 0.1880],
-        [0.3010, 0.7450, 0.9330],
-        [0.6350, 0.0780, 0.1840]
-    ]
-
     global _colors
     global _colorIndices
 
     if not _colors: # only define the global colors if they are not already defined
+        _rgbcolors = [ # some rgb colors (taken from MATLABs default palette)
+            [0, 0.4470, 0.7410],
+            [0.8500, 0.3250, 0.0980],
+            [0.9290, 0.6940, 0.1250],
+            [0.4940, 0.1840, 0.5560],
+            [0.4660, 0.6740, 0.1880],
+            [0.3010, 0.7450, 0.9330],
+            [0.6350, 0.0780, 0.1840]
+        ]
+
+        # calling TColor.GetFreeColorIndex returns a different index only after
+        # it has actually been 'used', so that calling it twice in the tuple creation
+        # is OK
         _getNewIdx = TColor.GetFreeColorIndex # bind function call to local variable for less typing
         _colors = [(_getNewIdx(), TColor(_getNewIdx(), _rgbcolors[i][0], _rgbcolors[i][1], _rgbcolors[i][2]))
                    for i in range(len(_rgbcolors))]
         _colorIndices = [i[0] for i in _colors]
 
     return _colorIndices
+
 
 def get_palette_min():
     """Get the color at the minimum of the currently set palette"""
